ef03bd344d315ece0f45ebd9a73803df687244c8
gheeeeeeee <gheoputra55@gmail.com>
2024-06-03 15:09:18 +0700

    Manual exception

---
+++ Manual_Exception/Manual_Exception.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	try {
+		cout << "selamat belajar di prodi TI UMY" << endl;
+		//throw 0.5; //melemparkan sebuuah integer maka
+		cout << "Pertnyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << " Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		cout << "Default Pengecualian dieksekusi" << end;
+	}
+	return 0;
+}
+
