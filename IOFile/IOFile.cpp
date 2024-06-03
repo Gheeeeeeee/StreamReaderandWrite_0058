3b6b6a385169c442956984dae65b6a18e664932e
gheeeeeeee <gheoputra55@gmail.com>
2024-06-03 14:38:12 +0700

    line 39-51 conditional statement apabila file ada atau tidak ditemukan

--- IOFile/IOFile.cpp
+++ IOFile/IOFile.cpp
@@ -44,8 +44,12 @@ int main()
 			//dan tampilkan disini
 			cout << baris << '\n';
 		}
+		//tutp file tersebut setelah selesai
+		infile.close();
 	}
-
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "unable to open file";
+	return 0;
 }
 
 
